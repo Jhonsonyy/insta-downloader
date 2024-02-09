b1a40af4182ce54bf9550dffc3156354352bea10
Jhonsonyy <134149091+Jhonsonyy@users.noreply.github.com>
2024-02-09 10:38:26 -0800

    app.js

--- app.js
+++ app.js
@@ -3,47 +3,35 @@ const axios = require('axios');
 const path = require('path')
 const instagramDl = require('@sasmeee/igdl');
 const app = express();
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 
 const dir = path.join(__dirname, 'public');
+let final_obj = ""
+let getdata = "";
+let thumbnail = "";
 
 
 app.use(express.static(dir));
 
 
+app.post('/downloadpage', async (req, res) => {
+  getdata = req.body;
+})
 
-app.post('/download', async (req, res) => {
-  const global_link = req.body.url;
-  if (global_link !== '' && global_link.includes('instagram')) {
-    try {
-      const download_obj = await instagramDl(global_link);
-      const download_Link = download_obj[0].download_link;
-      // Use axios to download the video
-      const response = await axios.get(download_Link, { responseType: 'stream' });
-
-      // Set the appropriate headers for the file download
-      res.setHeader('Content-Disposition', 'attachment; filename=InstaGrabX_video.mp4');
-      res.setHeader('Content-Type', 'video/mp4');
-     res.setHeader('Content-Length', response.headers['content-length']);
-      
-      // Pipe the response data to the client's response
-      response.data.pipe(res);
-
-
-
-// Call the function to start the 5-second timer
-    } catch (error) {
-      res.status(500).send('Error downloading video.');
-    }
-  } else {
-    res.status(400).send('Invalid Instagram URL.');
+app.get('/object_grab', async (req, res) => {
+  if (getdata.key !== '' && getdata.key.includes('instagram')) {
+    const download_obj = await instagramDl(getdata.key)
+    final_obj = download_obj[0]
+    res.send(final_obj)
   }
-});
+
+})
+
+
 
 app.listen(port, () => {
-
+  console.log("running on port: ", port)
 });
-
-
