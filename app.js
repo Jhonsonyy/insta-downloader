cb64376aea06a440abc141405973a3f92634fe62
Jhonsonyy <134149091+Jhonsonyy@users.noreply.github.com>
2024-02-10 14:00:41 -0800

    app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get('/object_grab', async (req, res) => {
 
 
 
-app.listen(port, 'jade-important-capybara.cyclic.app', () => {
+app.listen(port, 'reel-downloader.cyclic.app', () => {
   console.log("running on port: ", port)
 });
 
